e121791416d9c567d1de76608d49030f808a82e9
david <62183646+daviddotflac@users.noreply.github.com>
2021-04-10 10:47:52 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ class App extends Component {
       <Container>
         <h2>david.flac</h2>
         <p>
-          <strong>Links:</strong>
+          <span>Links:</span>
           <a className="link standalone-link" href="https://soundcloud.com/daviddotflac" target="_blank" rel="noopener noreferrer" title="https://soundcloud.com/daviddotflac">https://soundcloud.com/daviddotflac</a>
           
           <a className="link standalone-link" href="https://twitter.com/daviddotflac_" target="_blank" rel="noopener noreferrer" title="https://twitter.com/daviddotflac_">https://twitter.com/daviddotflac_</a>
